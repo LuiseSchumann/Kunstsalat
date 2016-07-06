60470088590fa47efe398c0eac81d0448ce37a4a
felixsewing <felixsewing@googlemail.com>
2016-07-06 16:18:58 +0200

    Sicherung
    für mich

--- javascript/java_index.js
+++ javascript/java_index.js
@@ -1,12 +1,14 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("sidenav").style.width = "230px";
+    document.getElementById("sidenav").style.width = "220px";
     /*document.getElementsByClassName("kopfbereich").style.display = 'none';*/
-    document.getElementById('test').style.display = 'none';
+    document.getElementById("test").style.display = 'none';
+    document.getElementById("test").style.width = "220px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
-    document.getElementById('test').style.display = 'block';
+    document.getElementById("test").style.display = 'block';
+    document.getElementById("test").style.width = "40px";
 }
