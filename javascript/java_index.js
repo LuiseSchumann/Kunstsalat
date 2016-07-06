f8e21552c6adb808c16247e65cdebe4868bba20f
felixsewing <felixsewing@googlemail.com>
2016-07-06 22:08:03 +0200

    so nochmal schön
    wuwuwuwwu!

--- javascript/java_index.js
+++ javascript/java_index.js
@@ -1,14 +1,13 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("sidenav").style.width = "220px";
     /*document.getElementsByClassName("kopfbereich").style.display = 'none';*/
-    document.getElementById("test").style.display = 'none';
-    document.getElementById("test").style.width = "220px";
+    document.getElementById("test").style.display = 'none'
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
-    document.getElementById("test").style.display = 'block';
-    document.getElementById("test").style.width = "40px";
+    setTimeout(function () {
+      document.getElementById("test").style.display = 'block';
+    }, 400);
+
 }
